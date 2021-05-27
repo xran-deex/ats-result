94b16fa71bff7c41c24632b4d02baa6dd1666f1d
Randy Valis <randy.valis@gmail.com>
2021-05-26 20:48:39 -0400

    WIP

--- tests/conanfile.py
+++ tests/conanfile.py
@@ -3,9 +3,6 @@ from atsconan import ATSConan
 class ATSConan(ATSConan):
     name = "ats-result-tests"
     version = "0.1.1"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "atsmake"
-    exports_sources = "*"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     requires = "ats-result/0.1.1@randy.valis/testing"
